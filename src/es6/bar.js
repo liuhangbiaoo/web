b70a9f4079198e820281738c692836767d47e0cc
liuhangbiao <liuhangbiaoo@gmail.com>
2017-05-09 14:14:32 +0800

    'uploade'

--- src/es6/bar.js
+++ src/es6/bar.js
@@ -1,7 +1,7 @@
-// import qw from '../qq.json';
-// var qe=require("../qq.json");
-// console.log("qw:"+qw);
-// console.log("qe:"+qe);
+import qw from '../data/qq.json';
+var qe=require("../data/qq.json");
+console.log("qw:"+qw);
+console.log("qe:"+qe);
 export default function bar() {
   document.body.style.backgroundColor="#093";
   let qq="xxxxxx";
